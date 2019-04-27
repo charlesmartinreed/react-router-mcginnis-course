efa33b52c2c8ca4eb6679382d7ef9bea7b9a515b
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-26 23:30:33 -0500

    Demonstrate basics of handling 404 pages and redirects with react router.

--- src/App.js
+++ src/App.js
@@ -1,147 +1,34 @@
 import React, { Component } from 'react';
-import { Route, Link, BrowserRouter as Router} from 'react-router-dom';
+import { Route, Link, Switch, Redirect, BrowserRouter as Router} from 'react-router-dom';
 
-const topics = [
-  {
-    name: 'React Router',
-    id: 'react-router',
-    description: 'Declarative, component based routing for React',
-    resources: [
-      {
-        name: 'URL Parameters',
-        id: 'url-parameters',
-        description: "URL parameters are parameters whose values are set dynamically in a page's URL. This allows a route to render the same component while passing that component the dynamic portion of the URL so it can change based off of it.",
-        url: 'https://tylermcginnis.com/react-router-url-parameters/'
-      },
-      {
-        name: 'Programmatically navigate',
-        id: 'programmatically-navigate',
-        description: "When building an app with React Router, eventually you'll run into the question of navigating programmatically. The goal of this post is to break down the correct approaches to programmatically navigating with React Router.",
-        url: 'https://tylermcginnis.com/react-router-programmatically-navigate/'
-      }
-    ]
-  },
-  {
-    name: 'React.js',
-    id: 'reactjs',
-    description: 'A JavaScript library for building user interfaces',
-    resources: [
-      {
-        name: 'React Lifecycle Events',
-        id: 'react-lifecycle',
-        description: "React Lifecycle events allow you to tie into specific phases of a component's life cycle",
-        url: 'https://tylermcginnis.com/an-introduction-to-life-cycle-events-in-react-js/'
-      },
-      {
-        name: 'React AHA Moments',
-        id: 'react-aha',
-        description: "A collection of 'Aha' moments while learning React.",
-        url: 'https://tylermcginnis.com/react-aha-moments/'
-      }
-    ]
-  },
-  {
-    name: 'Functional Programming',
-    id: 'functional-programming',
-    description: 'In computer science, functional programming is a programming paradigm—a style of building the structure and elements of computer programs—that treats computation as the evaluation of mathematical functions and avoids changing-state and mutable data.',
-    resources: [
-      {
-        name: 'Imperative vs Declarative programming',
-        id: 'imperative-declarative',
-        description: 'A guide to understanding the difference between Imperative and Declarative programming.',
-        url: 'https://tylermcginnis.com/imperative-vs-declarative-programming/'
-      },
-      {
-        name: 'Building User Interfaces with Pure Functions and Function Composition',
-        id: 'fn-composition',
-        description: 'A guide to building UI with pure functions and function composition in React',
-        url: 'https://tylermcginnis.com/building-user-interfaces-with-pure-functions-and-function-composition-in-react-js/'
-      }
-    ]
-  }
-]
+const Home = () => <h1>Home</h1>
+const WillMatch = () => <h1>Matched!</h1>
 
-function Home() {
-	return (
-		<h1>
-			Home
-		</h1>
-	)
-}
+const NoMatch = ({location}) => (
+	<div>
+		<h3>No match for <code>{location.pathname}</code></h3>
+	</div>
+)
 
-// match /topics/topicId/subId
-function Resource( {match} ) {
-	const topic = topics.find(({id}) => id === match.params.topicId).resources.find(( {id}) => id === match.params.subId)
-
-	return (
-		<div>
-			<h3>{topic.name}</h3>
-			<p>{topic.description}</p>
-			<a href={topic.url}>More Info</a>
-		</div>
-	)
-}
-
-function Topic( {match} ) {
-	const topic = topics.find(({ id }) => id === match.params.topicId)
-
-	return (
-		<div>
-			<h2>{topic.name}</h2>
-			<p>{topic.description}</p>
-
-			<ul>
-				{topic.resources.map((sub) => (
-					<li key={sub.id}>
-						<Link to={`${match.url}/${sub.id}`}>
-							{sub.name}
-						</Link>
-					</li>
-				))}
-			</ul>
-
-			<hr />
-			<Route path={`${match.path}/:subId`} component={Resource} />
-		</div>
-	)
-}
-
-// - Match.path is used for building nested routes - with route
-// - Match.url is used for route matching inside of Links
-
-function Topics( {match} ) {
-	return (
-		<div>
-			<h1>Topics</h1>
-			<ul>
-				{topics.map(( { name, id} ) => (
-					<li key={id}>
-						<Link to={`${match.url}/${id}`}>{name}</Link>
-					</li>
-				))}
-			</ul>
-
-			<hr />
-			<Route path={`${match.path}/:topicId`} component={Topic} />
-		</div>
-	)
-}
-
-class App extends Component {
+ class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div style={{ width: 1000, margin: '0 auto' }}>
+				<div>
 					<ul>
 						<li><Link to='/'>Home</Link></li>
-						<li><Link to='/topics'>Topics</Link></li>
+						<li><Link to='/old-match'>Old Match, to be redirected</Link></li>
+						<li><Link to='will-match'>Will Match</Link></li>
+						<li><Link to='/will-not-match'>Will Not Match</Link></li>
+						<li><Link to='/also/will/not/match'>Also will not match</Link></li>
 					</ul>
 
-					<hr />
-
-					<Route exact path='/' component={Home} />
-					<Route path='/topics' component={Topics} />
-
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Redirect from='/old-match' to='/will-match' />
+						<Route path='/will-match' component={WillMatch} />
+						<Route component={NoMatch} />
+					</Switch>
 				</div>
 			</Router>
 		)
