aa6bf135b574a099507a112849065f286a024485
charlesmartinreed <charlesmartinreed@icloud.com>
2019-04-27 22:55:38 -0500

    Demonstrate basic example of composing custom Link component.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,51 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-// you can render more than one route at any point in your app
-const routes = [
-	{
-		path: '/',
-		exact: true,
-		sidebar: () => <div>home!</div>,
-		main: () => <h2>Home</h2>,
-	},
-	{
-		path: '/bubblegum',
-		sidebar: () => <div>bubblegum!</div>,
-		main: () => <h2>Bubblegum</h2>,
-	},
-	{
-		path: '/shoelaces',
-		sidebar: () => <div>shoelaces!</div>,
-		main: () => <h2>Shoelaces</h2>,
-	}
-]
+const Home = () => (
+	<div>
+			<h2>Home</h2>
+	</div>
+)
+
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+)
+
+// children renders this component whether there is a match or not
+// when path does not match, match is null
+const OldSchoolMenuLink = ({ children, to, exact}) => {
+	return (
+		<Route path={to} exact={exact} children={({match}) => (
+			<div className={match ? 'active' : ''}>
+				{match ? '>' : ''}
+				<Link to={to}>
+					{children}
+				</Link>
+			</div>
+		)}/>
+	)
+}
 
 // map over routes array, render either sidebar or main
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div style={{display: 'flex'}}>
-				<div style={{
-					padding: '10px',
-					width: '40%',
-					background: '#f0f0f0'
-				}}>
-					<ul style={{listStyleType: 'none', padding: 0}}>
-						<li><Link to='/'>Home</Link></li>
-						<li><Link to='/bubblegum'>Bubblegum</Link></li>
-						<li><Link to='/shoelaces'>Shoelaces</Link></li>
-					</ul>
+				<div>
+					<OldSchoolMenuLink exact={true} to='/'>
+						Home
+					</OldSchoolMenuLink>
 
-					{routes.map((route) => (
-						<Route
-						key={route.path} path={route.path} exact={route.exact} component={route.sidebar} />
-					))}
-				</div>
-					<div style={{flex: 1, padding: '30px'}}>
-						{routes.map(({ path, exact, main}) => (
-							<Route
-								key={path}
-								path={path}
-								exact={exact}
-								component={main}
-							/>
-						))}
-					</div>
+					<OldSchoolMenuLink to='/about'>
+						About
+					</OldSchoolMenuLink>
+
+					<hr />
+
+					<Route exact path='/' component={Home} />
+					<Route path='/about' component={About} />
 				</div>
 			</Router>
 		)
